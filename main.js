f8f285ffaaecead09800ccf19ba4f580126c8d78
andkymon <andkymon@gmail.com>
2025-02-11 18:17:30 +0800

    test: add tests for utility functions & calculator

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-export function sum(a, b) {
-    return a + b;
-}
+export const testFunctions = (() => {
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    function reverseString(string) {
+        return string.split("").reverse().join("");
+    }
+
+    const calculator = {
+        'add' : (a, b) => {
+            return Number(a) + Number(b);
+        },
+
+        'subtract' : (a, b) => {
+            return a - b;
+        },
+
+        'multiply' : (a, b) => {
+            return a * b;
+        },
+
+        'divide' : (a, b) => {
+            return a / b;
+        }
+    }
+
+    return {
+        capitalize,
+        reverseString,
+        calculator
+    }
+})();
--- main.test.js
+++ main.test.js
@@ -1,5 +1,60 @@
-import { sum } from "./main.js";
+import { testFunctions } from './main';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('capitalize() method', () => {
+    test('Capitalize first letter of all lowercase string', () => {
+        expect(testFunctions.capitalize('kyle')).toBe('Kyle');
+    });
+
+    test('Capitalize first letter of all uppercase string', () => { 
+        expect(testFunctions.capitalize('KYLE')).toBe('KYLE');
+    });
+        
+    test('Capitalize first letter of mixed case string', () => {
+        expect(testFunctions.capitalize('kyLE')).toBe('KyLE');
+    });
 });
+
+describe('reverseString() method', () => {
+    test("Reverse an all lowercase string", () => {
+        expect(testFunctions.reverseString('kyle')).toBe('elyk');
+    });
+
+    test("Reverse a mixed case string", () => {
+        expect(testFunctions.reverseString('KyLe')).toBe('eLyK');
+    });
+});
+
+describe('calculator object', () => {
+    test("Add", () => {
+        expect(testFunctions.calculator.add(123, 456)).toBe(579);
+    });
+
+    test("Add string values", () => {
+        expect(testFunctions.calculator.add("123", "456")).toBe(579);
+    });
+
+    test("Subtract", () => {
+        expect(testFunctions.calculator.subtract(579, 456)).toBe(123);
+    });
+
+    test("Subtract string values", () => {
+        expect(testFunctions.calculator.subtract("222", "111")).toBe(111);
+    });
+
+    test("Multiply", () => {
+        expect(testFunctions.calculator.multiply(5, 10)).toBe(50);
+    });
+
+    test("Multiply string values", () => {
+        expect(testFunctions.calculator.multiply("6", "6")).toBe(36);
+    });
+
+    test("Divide", () => {
+        expect(testFunctions.calculator.divide(50, 10)).toBe(5);
+    });
+
+    test("Divide", () => {
+        expect(testFunctions.calculator.divide("36", "6")).toBe(6);
+    });
+});
+
